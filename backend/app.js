668e7241cd230c0f8abb97a5c6123e71a2aad139
FlorianV012 <fvilmot@gmail.com>
2021-10-07 15:08:15 +0200

    Connexion à la base de données

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,37 @@
 const express = require('express');
+require('dotenv').config();
+const mysql = require('mysql2');
+// const { Sequelize } = require('sequelize');
+
+
+// connexion à la BD avec mysql
+
+const db = mysql.createConnection({
+  host: process.env.MySQL_DB_host,
+  user: process.env.MySQL_DB_user,
+  password: process.env.MySQL_DB_password,
+  database: process.env.MySQL_DB
+});
+
+db.connect((err) => {
+  if (err) { throw err; }
+  console.log("Connecté à la base de données MySQL!");
+});
+
+/*
+// connexion à la BD avec sequelize
+const sequelize = new Sequelize(process.env.MySQL_DB, process.env.MySQL_DB_user, process.env.MySQL_DB_password, {
+  dialect: "mysql",
+  host: process.env.MySQL_DB_host
+ });
+ 
+ try {
+  sequelize.authenticate();
+  console.log('Connecté à la base de données MySQL!');
+} catch (error) {
+  console.error('Impossible de se connecter, erreur suivante :', error);
+}
+*/
 
 const app = express();
 
